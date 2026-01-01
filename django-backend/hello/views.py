e836bef24822b3f1ad19b327b03a0e028a8e3bf6
chugyoyo <chugyoyo@gmail.com>
2026-01-01 18:25:15 +0800

    django

--- django-backend/hello/views.py
+++ django-backend/hello/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import User
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    users = User.objects.all()
+    data = ", ".join([u.name for u in users])
+    return HttpResponse(f"Users: {data}")
 
